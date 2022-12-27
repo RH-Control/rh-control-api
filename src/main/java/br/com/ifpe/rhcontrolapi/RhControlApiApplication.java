70601cc854d840bd7d5c41a6652c70e916253714
Matheus Montenegro <matheusalbuquerquemontenegro@hotmail.com>
2022-12-27 19:09:50 -0300

    initial config

---
+++ src/main/java/br/com/ifpe/rhcontrolapi/RhControlApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.rhcontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/rhcontrolapi/RhControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.rhcontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
